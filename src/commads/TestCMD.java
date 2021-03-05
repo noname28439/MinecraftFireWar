1ea953418e7b3ae17d8bf7883f55fa243d10ac5e
noname28439 <dohle.leon@web.de>
2021-03-05 21:39:11 +0100

    Added VistitWorld Command

--- src/commads/TestCMD.java
+++ src/commads/TestCMD.java
@@ -1,5 +1,8 @@
 package commads;
 
+import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
+import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -15,20 +18,31 @@ public class TestCMD implements CommandExecutor {
 		
 		Player p = (Player)sender;
 		
-		p.sendMessage("Test geklappt, du Schluri!");
 		
 		
 		
 		
-		if(p.isOp())
-		if(args.length==1) {
-			if(args[0].equalsIgnoreCase("skip"))
-				if(GameStateManager.getCurrentGameState().getID()==GameStateManager.BuildState) {
-					BuildState state = (BuildState) GameStateManager.getCurrentGameState();
-					state.seconds = BuildState.BuildTimeSec-10;
-				}
-			if(args[0].equalsIgnoreCase("start"))
-				GameStateManager.setGameState(new BuildState());
+		
+		if(p.isOp()) {
+			if(args.length==1) {
+				if(args[0].equalsIgnoreCase("skip"))
+					if(GameStateManager.getCurrentGameState().getID()==GameStateManager.BuildState) {
+						BuildState state = (BuildState) GameStateManager.getCurrentGameState();
+						state.seconds = BuildState.BuildTimeSec-10;
+					}
+				if(args[0].equalsIgnoreCase("start"))
+					GameStateManager.setGameState(new BuildState());
+				
+				
+				
+			}
+		}
+		
+		if(args.length==2) {
+			if(args[0].equalsIgnoreCase("vv")) {
+				p.setGameMode(GameMode.SPECTATOR);
+				p.teleport(new Location(Bukkit.getWorld(args[1]), 0, 100, 0));
+			}
 		}
 			
 					
