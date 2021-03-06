651be375ec3b6a2401a65abba38d99ce28fe557a
noname28439 <dohle.leon@web.de>
2021-03-06 15:56:53 +0100

    BuildCommand only in BuildPhase, BuildPhase start Items

--- src/commads/BuildCMD.java
+++ src/commads/BuildCMD.java
@@ -7,6 +7,7 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 import game.BuildState;
+import game.GameStateManager;
 import game.InGameBuildTools;
 
 public class BuildCMD implements CommandExecutor {
@@ -16,18 +17,25 @@ public class BuildCMD implements CommandExecutor {
 		
 		Player p = (Player)sender;
 		
-		if(BuildState.playerBuildStartPoints.get(p)==null||BuildState.playerBuildEndPoints.get(p)==null) {
-			p.sendMessage(ChatColor.RED+"Du musst erst einen Bereich markieren!");
-			return false;
+		if(GameStateManager.getCurrentGameState().getID()==GameStateManager.BuildState || p.isOp()) {
+			
+			if(BuildState.playerBuildStartPoints.get(p)==null||BuildState.playerBuildEndPoints.get(p)==null) {
+				p.sendMessage(ChatColor.RED+"Du musst erst einen Bereich markieren!");
+				return false;
+			}
+			
+			boolean finished = InGameBuildTools.fillRect(p, BuildState.playerBuildStartPoints.get(p), BuildState.playerBuildEndPoints.get(p));
+			if(finished) {
+				BuildState.playerBuildStartPoints.put(p, null);
+				BuildState.playerBuildEndPoints.put(p, null);
+				p.sendMessage(ChatColor.GREEN+"Build finished!");
+			}
+			
+		}else {
+			p.sendMessage(ChatColor.RED+"Du kannst diesen Befehl nur in der Bauphase einsetzen!");
 		}
-		
-		boolean finished = InGameBuildTools.fillRect(p, BuildState.playerBuildStartPoints.get(p), BuildState.playerBuildEndPoints.get(p));
-		if(finished) {
-			BuildState.playerBuildStartPoints.put(p, null);
-			BuildState.playerBuildEndPoints.put(p, null);
-			p.sendMessage(ChatColor.GREEN+"Build finished!");
-		}
-		
+			
+
 		
 		return false;
 	}
