137204235ce33f5f00b137a8dd313143b4ecfa5d
noname28439 <dohle.leon@web.de>
2021-03-09 12:07:13 +0100

    KeepInventory Option, WorldFireTick off after Game Ends

--- src/game/BuildState.java
+++ src/game/BuildState.java
@@ -159,6 +159,7 @@ public class BuildState extends GameState{
 		Bukkit.getWorld(worldName).setGameRuleValue("fallDamage", "false");
 		Bukkit.getWorld(worldName).setGameRuleValue("doFireTick", "false");
 		Bukkit.getWorld(worldName).setGameRuleValue("doWeatherCycle", "false");
+		Bukkit.getWorld(worldName).setGameRuleValue("keepInventory", String.valueOf(Settings.KeepInventory));
 		Bukkit.getWorld(worldName).getWorldBorder().setSize(100.0);
 		System.out.println("Teleporting Players...");
 		for(Player cp : Bukkit.getOnlinePlayers())
